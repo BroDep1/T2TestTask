e4e62669b008245c54dca36ee6b2c8e084715797
denmonster <denmonster08@gmail.com>
2025-11-01 15:52:22 +0300

    init

---
+++ src/main/java/org/example/t2testtask/T2TestTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.t2testtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2TestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2TestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/t2testtask/T2TestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.t2testtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2TestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
